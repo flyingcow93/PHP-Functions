ed3deebab26cd360a8e36d0d92ec1f7e642bc94f
flyingcow93 <bwl3@njit.edu>
2015-04-02 14:53:14 -0400

    Inverf
    Inverse Error Function

---
+++ inverf.php
@@ -0,0 +1,7 @@
+<?
+function inverf($x) {
+		$a=0.147;
+		$sign=( $x > 0 ) ? 1 : ( ( $x < 0 ) ? -1 : 0 );
+		return $sign*sqrt(sqrt(pow(((2/(pi()*$a))+((log(1-pow($x, 2)))/2)), 2) - ((log(1-pow($x, 2)))/$a) )-((2/(pi()*$a))+(log(1-pow($x, 2)))/2));
+	}
+	?>
